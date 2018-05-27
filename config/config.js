7c547bd5b875d6662f17a408a03aa46b7cedc1a9
yttrium7 <yttrium.l@pm.me>
2018-05-28 06:46:12 +1000

    delete AWS API

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    port: 3000,
-    mongodb:'mongodb://Yttrium:cHENNIGHT7007@ds159129.mlab.com:59129/paprica',
-    AWS_KEY_ID: 'AKIAJHGHPQHPKVI3WCMQ',
-    AWS_SECRET_KEY: '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'
-};
