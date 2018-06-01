112c69a43d6f0f7069fd5f0155cfce4181892546
yttrium7 <37195303+yttrium7@users.noreply.github.com>
2018-06-02 06:00:05 +1000

    AWS account has suspended
    the assets for website are currently unavaliable

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 3000,
     mongodb:'mongodb://Yttrium:cHENNIGHT7007@ds159129.mlab.com:59129/paprica',
-    AWS_KEY_ID: 'AKIAINYRDXOUERI7N3WQ',
-    AWS_SECRET_KEY: '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'
+    AWS_KEY_ID: '',
+    AWS_SECRET_KEY: ''
 };
